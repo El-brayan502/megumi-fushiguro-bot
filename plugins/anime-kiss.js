866bcb760b2b5ff54d3821cb36c05ad93304916b
Soy Neji <brayanphonkponk@gmail.com>
2026-02-14 19:49:38 -0600

    Actualizar anime-kiss.js

--- plugins/anime-kiss.js
+++ plugins/anime-kiss.js
@@ -3,13 +3,20 @@ import path from 'path'
 
 let handler = async (m, { conn, usedPrefix }) => {
     let who = m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.quoted ? m.quoted.sender : m.sender)
-    let name = conn.getName(who)
-    let name2 = conn.getName(m.sender)
 
-    let str = m.mentionedJid.length > 0 || m.quoted 
-        ? `\`${name2}\` beso a \`${name || who}\` ( ˘ ³˘)♥` 
-        : `\`${name2}\` se besó a sí mismo/a ( ˘ ³˘)♥`
-    
+    who = conn.decodeJid(who) 
+    let sender = conn.decodeJid(m.sender)
+
+    let name = conn.getName(who)
+    let name2 = conn.getName(sender)
+
+    let displayName = name.includes('@') ? name.split('@')[0] : name
+    let displayName2 = name2.includes('@') ? name2.split('@')[0] : name2
+
+    let str = (m.mentionedJid.length > 0 || m.quoted) 
+        ? `\`${displayName2}\` beso a \`${displayName}\` ( ˘ ³˘)♥` 
+        : `\`${displayName2}\` se besó a sí mismo/a ( ˘ ³˘)♥`
+
     if (m.isGroup) {
         let pp = 'https://media.tenor.com/_8oadF3hZwIAAAPo/kiss.mp4'
         let pp2 = 'https://media.tenor.com/cQzRWAWrN6kAAAPo/ichigo-hiro.mp4'
@@ -19,11 +26,16 @@ let handler = async (m, { conn, usedPrefix }) => {
         let pp6 = 'https://media.tenor.com/ZDqsYLDQzIUAAAPo/shirayuki-zen-kiss-anime.mp4'
         let pp7 = 'https://media.tenor.com/LrKmxrDxJN0AAAPo/love-cheek.mp4'
         let pp8 = 'https://media.tenor.com/lyuW54_wDU0AAAPo/kiss-anime.mp4'
-        
+
         const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8]
         const video = videos[Math.floor(Math.random() * videos.length)]
-        
-        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, ptt: true, mentions: [who] }, { quoted: m })
+
+        conn.sendMessage(m.chat, { 
+            video: { url: video }, 
+            gifPlayback: true, 
+            caption: str, 
+            mentions: [who, sender] 
+        }, { quoted: m })
     }
 }
 
